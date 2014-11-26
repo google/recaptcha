47eb23a6357a16f3cb0be8c6e1e768f2378dc3dd
yingliu-google <yingliu-google@users.noreply.github.com>
2014-11-25 16:33:37 -0800

    Create example-captcha.php

---
+++ php/example-captcha.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Sample PHP code to use reCAPTCHA V2.
+ *
+ * @copyright Copyright (c) 2014, Google Inc.
+ * @link      http://www.google.com/recaptcha
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+require_once "recaptchalib.php";
+
+// Register API keys at https://www.google.com/recaptcha/admin
+$siteKey = "";
+$secret = "";
+// TODO: provide a link to 40+ languages supported in reCAPTCHA.
+$lang = "en";
+
+// The response from reCAPTCHA
+$resp = null;
+// The error code from reCAPTCHA, if any
+$error = null;
+
+$reCaptcha = new ReCaptcha($secret);
+
+// Was there a reCAPTCHA response?
+if ($_POST["g-recaptcha-response"]) {
+    $resp = $reCaptcha->verifyResponse(
+        $_SERVER["REMOTE_ADDR"],
+        $_POST["g-recaptcha-response"]
+    );
+}
+?>
+<html>
+  <head><title>reCAPTCHA Example</title></head>
+  <body>
+<?php
+if ($resp != null && $resp->success) {
+    echo "You got it!";
+}
+?>
+    <form action="?" method="post">
+      <div class="g-recaptcha" data-sitekey="<?php echo $siteKey;?>"></div>
+      <script type="text/javascript"
+          src="https://www.google.com/recaptcha/api.js?hl=<?php echo $lang;?>">
+      </script>
+      <br/>
+      <input type="submit" value="submit" />
+    </form>
+  </body>
+</html>
