b3a67e0cfead0c968e418e9d4f90c85a40f9bb9d
Bo-Yi Wu <appleboy.tw@gmail.com>
2014-12-09 00:12:48 +0800

    check array index exist.

--- php/recaptchalib.php
+++ php/recaptchalib.php
@@ -130,11 +130,9 @@ class ReCaptcha
             $recaptchaResponse->success = true;
         } else {
             $recaptchaResponse->success = false;
-            $recaptchaResponse->errorCodes = $answers [error-codes];
+            $recaptchaResponse->errorCodes = (isset($answers['error-codes'])) ? $answers['error-codes'] : null;
         }
 
         return $recaptchaResponse;
     }
 }
-
-?>
