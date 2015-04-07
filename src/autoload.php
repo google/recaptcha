b1b50dec2b903d585e1fcdd942da6839e6c7a4c7
Michael Orlitzky <michael@orlitzky.com>
2015-04-07 14:17:47 -0400

    Prevent the autoloader from loading anything outside the "ReCaptcha" namespace.

--- src/autoload.php
+++ src/autoload.php
@@ -6,6 +6,13 @@
  */
 
 spl_autoload_register(function ($class) {
+    if (substr($class, 0, 10) !== 'ReCaptcha\\') {
+      /* If the class does not lie under the "ReCaptcha" namespace,
+       * then we can exit immediately.
+       */
+      return;
+    }
+
     /* All of the classes have names like "ReCaptcha\Foo", so we need
      * to replace the backslashes with frontslashes if we want the
      * name to map directly to a location in the filesystem.
