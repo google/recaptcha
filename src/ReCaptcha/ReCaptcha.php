614f25a9038be4f3f2da7cbfd778dc5b357d2419
Rowan Merewood <rowan@merewood.org>
2020-03-31 18:50:54 +0100

    Minor version bump

--- src/ReCaptcha/ReCaptcha.php
+++ src/ReCaptcha/ReCaptcha.php
@@ -43,7 +43,7 @@ class ReCaptcha
      * Version of this client library.
      * @const string
      */
-    const VERSION = 'php_1.2.3';
+    const VERSION = 'php_1.2.4';
 
     /**
      * URL for reCAPTCHA siteverify API
