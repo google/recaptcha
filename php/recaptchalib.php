ae92d1a4eff376590f97547263fba1edbbe63a39
phanvuars <phanvuars@gmail.com>
2024-07-29 20:54:04 +0700

    Fix binding key of success and error-codes

--- php/recaptchalib.php
+++ php/recaptchalib.php
@@ -126,11 +126,11 @@ class ReCaptcha
         $answers = json_decode($getResponse, true);
         $recaptchaResponse = new ReCaptchaResponse();
 
-        if (trim($answers [success]) == true) {
+        if (trim($answers ['success']) == true) {
             $recaptchaResponse->success = true;
         } else {
             $recaptchaResponse->success = false;
-            $recaptchaResponse->errorCodes = $answers [error-codes];
+            $recaptchaResponse->errorCodes = $answers ['error-codes'];
         }
 
         return $recaptchaResponse;
